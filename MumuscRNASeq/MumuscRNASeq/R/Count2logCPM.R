894c56bd1b8dddc5bd06a73f64418744a4f6e920
Junqiang Wang <51161903+JunqiangWang@users.noreply.github.com>
2022-09-22 20:14:05 -0400

    Update Count2logCPM.R

--- MumuscRNASeq/MumuscRNASeq/R/Count2logCPM.R
+++ MumuscRNASeq/MumuscRNASeq/R/Count2logCPM.R
@@ -1,24 +1,4 @@
 
-#' This function convert count matrix to log2CPM matrix
-#' @param count The count matrix
-#' @return The log2CPM matrix
-#' @author Junqiang Wang
-#' @export
-Log2CPM_OLD<-function(count){
-
-  count <- apply(count,2,function(x){
-    y <- 1e6*x/sum(x) + 1
-    z <- log2(y)
-    return(z)
-  })
-
-  count<-round(count, digits=2)
-
-return(count)
-
-}
-
-
 
 #' This function convert count matrix to log2CPM matrix
 #' @param count The count matrix
